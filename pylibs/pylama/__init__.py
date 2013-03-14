1c5a6875db82fc1fd6039ec6372c33681932723c
Kirill Klenov <horneds@gmail.com>
2013-03-14 19:38:51 +0800

    Migrate to pylama

---
+++ pylibs/pylama/__init__.py
@@ -0,0 +1,8 @@
+" pylama -- Python code audit. "
+
+version_info = (0, 2, 1)
+
+__version__ = version = '.'.join(map(str, version_info))
+__project__ = __name__
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "GNU LGPL"
