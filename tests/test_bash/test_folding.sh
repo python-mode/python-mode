f0b58fae1debec65d45a51715c6aa96214a88490
Felipe M. Vieira <fmv1992@gmail.com>
2018-02-06 20:48:38 -0200

    disable folding3.vim test temporarily (gh issue #849)

--- tests/test_bash/test_folding.sh
+++ tests/test_bash/test_folding.sh
@@ -11,8 +11,9 @@ source ./test_helpers_bash/test_prepare_between_tests.sh
 vim -i NONE -u $VIM_TEST_VIMRC -c "source ./test_procedures_vimscript/folding2.vim" $VIM_DISPOSABLE_PYFILE > /dev/null
 R2=$?
 source ./test_helpers_bash/test_prepare_between_tests.sh
-vim -i NONE -u $VIM_TEST_VIMRC -c "source ./test_procedures_vimscript/folding3.vim" $VIM_DISPOSABLE_PYFILE > /dev/null
-R3=$?
+# TODO: enable folding3.vim script back.
+# vim -i NONE -u $VIM_TEST_VIMRC -c "source ./test_procedures_vimscript/folding3.vim" $VIM_DISPOSABLE_PYFILE > /dev/null
+# R3=$?
 source ./test_helpers_bash/test_prepare_between_tests.sh
 vim -i NONE -u $VIM_TEST_VIMRC -c "source ./test_procedures_vimscript/folding4.vim" $VIM_DISPOSABLE_PYFILE > /dev/null
 R4=$?
@@ -24,9 +25,9 @@ then
 elif [[ "$R2" -ne 0 ]]
 then
     exit 2
-elif [[ "$R3" -ne 0 ]]
-then
-    exit 3
+# elif [[ "$R3" -ne 0 ]]
+# then
+#     exit 3
 elif [[ "$R4" -ne 0 ]]
 then
     exit 4
