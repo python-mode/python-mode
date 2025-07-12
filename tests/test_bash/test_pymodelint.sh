0e26bb329216229c666af80233769d70c083b4ea
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2025-07-12 10:37:48 -0300

    Update tests to work within Docker test suite

--- tests/test_bash/test_pymodelint.sh
+++ tests/test_bash/test_pymodelint.sh
@@ -3,13 +3,15 @@
 # TODO XXX: improve python-mode testing asap.
 # Test all python commands.
 
-# Source file.
-set +e
-# vim --clean -i NONE -u "${VIM_TEST_VIMRC}" -c "source ./test_procedures_vimscript/pymodelint.vim" "${VIM_DISPOSABLE_PYFILE}" >> "${VIM_OUTPUT_FILE}" 2>&1
-CONTENT="$(vim --clean -i NONE -u "${VIM_TEST_VIMRC}" -c "source ./test_procedures_vimscript/pymodeversion.vim" "${VIM_DISPOSABLE_PYFILE}" 2>&1)"
-RETURN_CODE=$?
-echo -e "${CONTENT}" >> "${VIM_OUTPUT_FILE}"
-set -e
+function test_pymodelint() {
+    # Source file.
+    CONTENT="$(${VIM_BINARY:-vim} --not-a-term --clean -i NONE -u "${VIM_TEST_VIMRC}" -c "source ./test_procedures_vimscript/pymodelint.vim" "${VIM_DISPOSABLE_PYFILE}" 2>&1)"
+    RETURN_CODE=$?
+    echo -e "${CONTENT}" >> "${VIM_OUTPUT_FILE}"
 
-exit ${RETURN_CODE}
+    return ${RETURN_CODE}
+}
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    test_pymodelint
+fi
 # vim: set fileformat=unix filetype=sh wrap tw=0 :
--- tests/test_helpers_bash/test_prepare_between_tests.sh
+++ tests/test_helpers_bash/test_prepare_between_tests.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
 # Prepare tests.
-set +e
 if [ -f "${VIM_DISPOSABLE_PYFILE}" ]; then
     rm "${VIM_DISPOSABLE_PYFILE}"
 fi
-VIM_DISPOSABLE_PYFILE="$(mktemp /tmp/pymode.tmpfile.XXXXXXXXXX.py)"
+VIM_DISPOSABLE_PYFILE="/tmp/pymode.tmpfile.$(date +%s).py"
 export VIM_DISPOSABLE_PYFILE
-set -e
-touch "${VIM_DISPOSABLE_PYFILE}"
 
+touch "${VIM_DISPOSABLE_PYFILE}"
 # vim: set fileformat=unix filetype=sh wrap tw=0 :
